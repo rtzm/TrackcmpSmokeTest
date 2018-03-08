de7450830a3b32f5670b4e1789438976d56783db
Simon Swartzman <sswartzman@activecampaign.com>
2018-03-08 10:18:00 -0600

    Add event scripts to pages

--- control/eventTest.php
+++ control/eventTest.php
@@ -1,1 +1,29 @@
 <?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://trackcmp.net/event");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+	"actid" => "999929138",
+	"key" => "31b10ac323f6b43c4507f7d9551b7684f092703c",
+	"event" => "MANUAL_TEST",
+	"eventdata" => "TEST_VALUE",
+	"visit" => json_encode(array(
+		// If you have an email address, assign it here.
+		"email" => "kibaf@gmail.com",
+	)),
+));
+
+$result = curl_exec($curl);
+if ($result !== false) {
+	$result = json_decode($result);
+	if ($result->success) {
+		echo 'Success! ';
+	} else {
+		echo 'Error! ';
+	}
+
+	echo $result->message;
+} else {
+	echo 'cURL failed to run: ', curl_error($curl);
+}
--- staging/eventTest.php
+++ staging/eventTest.php
@@ -1,1 +1,29 @@
 <?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://trackcmp.net/event");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+	"actid" => "88928894",
+	"key" => "",
+	"event" => "MANUAL_TEST",
+	"eventdata" => "TEST_VALUE",
+	"visit" => json_encode(array(
+		// If you have an email address, assign it here.
+		"email" => "tatymasyv@gmail.com",
+	)),
+));
+
+$result = curl_exec($curl);
+if ($result !== false) {
+	$result = json_decode($result);
+	if ($result->success) {
+		echo 'Success! ';
+	} else {
+		echo 'Error! ';
+	}
+
+	echo $result->message;
+} else {
+	echo 'cURL failed to run: ', curl_error($curl);
+}
