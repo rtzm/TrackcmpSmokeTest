c199e9c5948d08700b56a303fc600bf100c462ac
Simon Swartzman <sswartzman@activecampaign.com>
2018-03-09 11:15:01 -0600

    Point to staging servers for trackcmp

--- staging/eventTest.php
+++ staging/eventTest.php
@@ -1,6 +1,6 @@
 <?php
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://trackcmp.net/event");
+curl_setopt($curl, CURLOPT_URL, "https://trackcmp.staging.app-us1.com/event");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, array(
